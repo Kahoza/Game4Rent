06b55ba24b4d53a6adde664c8987a89c32171a7b
karenhoyos <karenuca@hotmail.com>
2018-03-21 18:04:24 +0100

    Skip authentication user for index and show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [ :show, :edit, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
 
 def index
