6e5eae20b651ed077a8a526a11ebd8bc555e39f1
karenhoyos <karenuca@hotmail.com>
2018-03-21 17:19:47 +0100

    Models, added validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :owner
+  validates :name presence: true
+  validates :description presence: true
+  validates :price presence: true
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,4 @@
 class Owner < ApplicationRecord
   has_many :games
+  validates :first_name presence: true
 end
