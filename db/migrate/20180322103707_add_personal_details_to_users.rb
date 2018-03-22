6a7f7e8aca8b30c54a73bb3cc254f8747fad27a9
karenhoyos <karenuca@hotmail.com>
2018-03-22 11:42:51 +0100

    Schema changed as info was added to users controller

---
+++ db/migrate/20180322103707_add_personal_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddPersonalDetailsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :integer
+  end
+end
