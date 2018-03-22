ebb7f8a610d299274880d24c5413918c02965622
karenhoyos <karenuca@hotmail.com>
2018-03-22 10:01:29 +0100

    Method update in Owner's controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,12 @@
 class OwnersController < ApplicationController
+
+def update
+  @owner.update(owner_params)
+end
+
+private
+
+def owner_params
+  params.require(:owner).permit(:first_name, :last_name, :email, :phone_number)
+end
 end
