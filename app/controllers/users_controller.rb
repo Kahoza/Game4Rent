ffb6417eedb24a33759a882b5d729560dd2a9a02
karenhoyos <karenuca@hotmail.com>
2018-03-22 11:42:03 +0100

    Added User's controller, to replace the deleted Owner's controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def edit
+  @user = current_user
+end
+
+def update
+  @user = current_user
+  @user.update(user_params)
+  redirect_to root_path
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :phone_number, :email)
+end
+end
