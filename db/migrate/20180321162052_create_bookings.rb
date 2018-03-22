e7af954dc9b0325434bca41bf4bd2205a885ea7e
karenhoyos <karenuca@hotmail.com>
2018-03-22 11:55:09 +0100

    Changed owners for users

--- db/migrate/20180321162052_create_bookings.rb
+++ db/migrate/20180321162052_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
-      t.references :owner, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :game, foreign_key: true
 
       t.timestamps
