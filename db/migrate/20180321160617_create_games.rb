7be99d2ab59e333cd6ed198276920ae3bb83a086
karenhoyos <karenuca@hotmail.com>
2018-03-22 12:29:00 +0100

    Changed schema again to add finally user and removed owners

--- db/migrate/20180321160617_create_games.rb
+++ db/migrate/20180321160617_create_games.rb
@@ -5,7 +5,7 @@ class CreateGames < ActiveRecord::Migration[5.1]
       t.text :description
       t.integer :price
       t.string :photo
-      t.references :owner, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
