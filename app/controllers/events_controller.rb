2a1d2713faa6304474346f76dd966d683331c642
karenhoyos <karenuca@hotmail.com>
2018-03-28 10:31:59 +0200

    EventsController added methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,26 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+    @game = Game.find(params[:game_id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.game = Game.find(params[:game_id])
+    if @event.save
+      redirect to game_path(@event.game)
+    else
+      render :new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_date, :end_date)
+  end
 end
