edc3636840a03aa33a5236ef12b9eafcedc602a6
karenhoyos <karenuca@hotmail.com>
2018-03-28 12:59:51 +0200

    Modified games controller to add events

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,7 @@ class Game < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
+  has_many :events, dependent: :destroy
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
